06d7847cf6ed3a798ed53f94a9b52d057f5774b3
Enzo Campa <Enzocampa44@gmail.com>
2021-06-17 17:09:24 -0300

    Adição: menu superior Setas inferiores

--- js/index.js
+++ js/index.js
@@ -4,3 +4,16 @@ window.addEventListener('wheel', function(e) {
     if (e.deltaY > 0) item.scrollLeft += 100;
     else item.scrollLeft -= 100;
 });
+$('.dados>div').mouseenter((e)=>{
+    console.log(e)
+    $('.dados>div').css('border-radius','0')
+    $(".info-dados").css('background-color',$(e.target).css('background-color'))
+    $(".info-dados").css('opacity','1')
+    $(".info-dados").text(e.target.dataset.mensagem)
+})
+$('.info-dados').mouseleave((e)=>{
+    console.log(e)
+    $(".info-dados").css('opacity','0')
+    $('.dados>div:first-child').css('border-bottom-left-radius','10px')
+    $('.dados>div:last-child').css('border-bottom-right-radius','10px')
+})
